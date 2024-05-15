9b9802fe1e7cebb74be6ef4fe819111ebb224aa3
deepak022001 <studydipu@gmail.com>
2024-05-15 22:32:53 +0530

    Completed 2nd phase of project

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,11 +2,9 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import bodyParser from 'body-parser';
 import express from 'express';
-
 export async function initServer() {
   const app = express();
   app.use(bodyParser.json());
-
   const graphqlServer = new ApolloServer({
     typeDefs: `
       type Query {
