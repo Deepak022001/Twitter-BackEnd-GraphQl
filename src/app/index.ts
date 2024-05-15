79115c7d6dab89c89923f8cd69e72bbfde5ee9ee
deepak022001 <studydipu@gmail.com>
2024-05-15 12:41:52 +0530

    Added graphql and removed the errors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,28 +1,28 @@
-import express, { query } from 'express';
-import bodyParser from 'body-parser';
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/dist/esm/express4';
+import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser from 'body-parser';
+import express from 'express';
+
 export async function initServer() {
   const app = express();
   app.use(bodyParser.json());
+
   const graphqlServer = new ApolloServer({
     typeDefs: `
-    type Query{
-sayHello:String
-
-    }
-    type Muatation{
-
-    }
+      type Query {
+        sayHello: String
+      }
     `,
     resolvers: {
       Query: {
-        sayHello: () => `Hello from graphql server`,
+        sayHello: () => `Hello from GraphQL server`,
       },
-      Muatation: {},
     },
   });
+
   await graphqlServer.start();
   app.use('/graphql', expressMiddleware(graphqlServer));
   return app;
 }
+
+initServer();
